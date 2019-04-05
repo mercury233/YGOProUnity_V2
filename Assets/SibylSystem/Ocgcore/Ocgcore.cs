378ec3993a657d99b6f8cf315b0158f6b833c2ac
purerosefallen <78877@qq.com>
2019-04-05 16:09:28 +0800

    update side detect

--- Assets/SibylSystem/Ocgcore/Ocgcore.cs
+++ Assets/SibylSystem/Ocgcore/Ocgcore.cs
@@ -1302,6 +1302,7 @@ public class Ocgcore : ServantWithCardDescription
                 logicalClearChain();
                 surrended = false;
                 Program.I().room.duelEnded = false;
+                Program.I().room.joinWithReconnect = false;
                 turns = 0;
                 deckReserved = false;
                 keys.Insert(0, currentMessageIndex);
@@ -8818,6 +8819,7 @@ public class Ocgcore : ServantWithCardDescription
 
     public void onDuelResultConfirmed()
     {
+        Program.I().room.joinWithReconnect = false;
 
         if (Program.I().room.duelEnded == true || surrended || TcpHelper.tcpClient == null || TcpHelper.tcpClient.Connected == false)
         {
